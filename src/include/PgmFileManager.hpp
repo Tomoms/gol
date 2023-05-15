4febada79fe8d070abb46e0ba6d3084d4740fd6e
Tommaso Fonda <fondatommaso2@gmail.com>
2023-05-15 09:18:08 +0200

    PgmFileManager: Extend the API
    Add a constructor for reading an existing image, and getters.

--- src/include/PgmFileManager.hpp
+++ src/include/PgmFileManager.hpp
@@ -1,7 +1,10 @@
 #ifndef PGMFILEMANAGER_H
 #define PGMFILEMANAGER_H
 
+#include <string>
+
 #define PGM_MAX_VALUE	255
+#define PGM_HOLDER		const std::vector<std::vector<unsigned char>>
 
 class PgmFileManager
 {
@@ -11,9 +14,12 @@ private:
 	unsigned long rows, cols;
 
 public:
-	//PgmFileManager(std::string& filename); // open existing file ctor
+	PgmFileManager(std::string& filename); // open existing file ctor
 	PgmFileManager(std::string& filename, unsigned long size); // new file ctor
 	void write();
+	PGM_HOLDER read();
+	unsigned long get_rows();
+	unsigned long get_cols();
 };
 
 #endif
