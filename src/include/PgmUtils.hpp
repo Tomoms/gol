a5924779ae1f65617aec36dba648b5542bf35bee
Tommaso Fonda <fondatommaso2@gmail.com>
2023-08-07 11:18:33 +0200

    PgmUtils: Remove all functions, introduce write_header()

--- src/include/PgmUtils.hpp
+++ src/include/PgmUtils.hpp
@@ -9,14 +9,11 @@
 
 #define PGM_MAX_VALUE	255
 #define PGM_HOLDER		std::vector<unsigned char>
-#define SIZE_HOLDER		std::pair<unsigned long, unsigned long>
+#define SIZE_HOLDER		std::pair<unsigned long, unsigned long> // width (number of columns) and height (number of rows)
 
 namespace PgmUtils {
 
-SIZE_HOLDER read_size(std::string& filename);
-PGM_HOLDER read_image_data(std::string& filename, const SIZE_HOLDER& size);
-void write_image_data(std::string& filename, const SIZE_HOLDER& size, const PGM_HOLDER& image_data);
-PGM_HOLDER grid_to_image_data(std::vector<Cell>& grid);
+	void write_header(std::string& filename, SIZE_HOLDER& dimensions);
 
 }
 
